b92a72c2b5e4b75d9e3ffb7034a8c05b12c57c71
Ivan Nikitin <fenix661@gmail.com>
2015-10-28 03:27:24 +0300

    Create project

---
+++ OMM/OMM.h
@@ -0,0 +1,15 @@
+//
+//  OMM.h
+//  OMM
+//
+//  Created by Ivan Nikitin on 28/10/15.
+//  Copyright © 2015 Ivan Nikitin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OMM.
+FOUNDATION_EXPORT double OMMVersionNumber;
+
+//! Project version string for OMM.
+FOUNDATION_EXPORT const unsigned char OMMVersionString[];
---
+++ OMMDemo.playground/Contents.swift
@@ -0,0 +1,4 @@
+//: # OMM
+
+import OMM
+
