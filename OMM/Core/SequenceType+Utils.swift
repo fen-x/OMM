61f4dd6d6cfa0dec4ba667934e1010b8cd21e2ce
Ivan Nikitin <fenix661@gmail.com>
2015-11-06 01:45:28 +0300

    Add SequenceType extension

---
+++ OMM/Core/SequenceType+Utils.swift
@@ -0,0 +1,20 @@
+//
+//  SequenceType+Utils.swift
+//  OMM
+//
+//  Created by Ivan Nikitin on 04/11/15.
+//  Copyright © 2015 Ivan Nikitin. All rights reserved.
+//
+
+extension SequenceType {
+
+    @warn_unused_result
+    public func toDictionary<Key, Value>(@noescape transform: (Self.Generator.Element) throws -> (Key, Value)) rethrows -> [Key: Value] {
+        var result = [Key: Value]()
+        for (key, value) in try map(transform) {
+            result[key] = value
+        }
+        return result
+    }
+
+}
