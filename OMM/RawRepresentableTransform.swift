86c69f8609ac885049fb5bcff8449d9516e371dc
Ivan Nikitin <fenix661@gmail.com>
2015-11-10 01:52:15 +0300

    Add RawRepresentableTransform

---
+++ OMM/RawRepresentableTransform.swift
@@ -0,0 +1,27 @@
+//
+//  RawRepresentableTransform.swift
+//  OMM
+//
+//  Created by Ivan Nikitin on 10/11/15.
+//  Copyright © 2015 Ivan Nikitin. All rights reserved.
+//
+
+public
+struct RawRepresentableTransform<T: RawRepresentable where T.RawValue: ScalarType>: TransformType {
+
+    let defaultValue: T?
+
+    public
+    init(defaultValue: T? = nil) {
+        self.defaultValue = defaultValue
+    }
+
+    public
+    func applyToNode(node: NodeType) throws -> T {
+        guard let value = try T(rawValue: node.value()) ?? defaultValue else {
+            throw errorWithReason("Unexpected raw value")
+        }
+        return value
+    }
+
+}
