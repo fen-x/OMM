4ac52e42d920929be6d27cc6f0c5c282a9005b36
Ivan Nikitin <fenix661@gmail.com>
2015-11-10 02:08:02 +0300

    Add URLTransform

---
+++ OMM/URLTransform.swift
@@ -0,0 +1,25 @@
+//
+//  URLTransform.swift
+//  OMM
+//
+//  Created by Ivan Nikitin on 10/11/15.
+//  Copyright © 2015 Ivan Nikitin. All rights reserved.
+//
+
+import class Foundation.NSURL
+
+public
+struct URLTransform: TransformType {
+
+    public
+    init() { }
+
+    public
+    func applyToNode(node: NodeType) throws -> NSURL {
+        guard let URL = try NSURL(string: node.value()) else {
+            throw errorWithReason("Invalid URL")
+        }
+        return URL
+    }
+
+}
