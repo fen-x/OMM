d5fafb83499c6c39ac8abd72ddc3afb254a7cb7b
Ivan Nikitin <fenix661@gmail.com>
2015-11-10 02:12:39 +0300

    Add DateFromMillisecondsTransform

---
+++ OMM/DateFromMillisecondsTransform.swift
@@ -0,0 +1,25 @@
+//
+//  DateFromMillisecondsTransform.swift
+//  OMM
+//
+//  Created by Ivan Nikitin on 10/11/15.
+//  Copyright © 2015 Ivan Nikitin. All rights reserved.
+//
+
+import class  Foundation.NSDate
+import class  Foundation.NSNumber
+import struct Foundation.NSTimeInterval
+
+public
+struct DateFromMillisecondsTransform: TransformType {
+
+    public
+    init() { }
+
+    public
+    func applyToNode(node: NodeType) throws -> NSDate {
+        let number = try node.value() as NSNumber
+        return NSDate(timeIntervalSince1970: NSTimeInterval(number.longLongValue) / 1000)
+    }
+    
+}
