cac099fca346e5611828bb2551d091a84a63cf52
Ivan Nikitin <fenix661@gmail.com>
2015-11-10 02:09:38 +0300

    Add DateFromSecondsTransform

---
+++ OMM/DateFromSecondsTransform.swift
@@ -0,0 +1,22 @@
+//
+//  DateFromSecondsTransform.swift
+//  OMM
+//
+//  Created by Ivan Nikitin on 10/11/15.
+//  Copyright © 2015 Ivan Nikitin. All rights reserved.
+//
+
+import class Foundation.NSDate
+
+public
+struct DateFromSecondsTransform: TransformType {
+
+    public
+    init() { }
+
+    public
+    func applyToNode(node: NodeType) throws -> NSDate {
+        return try NSDate(timeIntervalSince1970: node.value())
+    }
+
+}
