b997acdcb06ee165c1ff306480af227a5fd4f76c
Ivan Nikitin <fenix661@gmail.com>
2015-11-11 02:46:16 +0300

    Populate demo playground with documentation

--- OMMDemo.playground/Contents.swift
+++ OMMDemo.playground/Contents.swift
@@ -1,9 +1,15 @@
 //: # OMM
+//:
+//: OMM is a **o**ne **m**ore **m**apper that helps to map JSON objects to Swift instances
+//:
 
 import OMM
 
-class Tweet: Mappable {
+//: ## Mappable Class
+//: There is an exmaple of how to make `Mappable` class below.
 
+class Tweet: Mappable {
+//: Unfortunately, Swift does not allow to throw error from initializer before all stored properties of a class instance initialized. That is why constatnt properties with non-optional type are not suitable.
     private(set)
     var id: Int64!
 
@@ -37,8 +43,22 @@ class Tweet: Mappable {
     private(set)
     var favoriteCount: Int!
 
+//: Provide initilizer that takes `NodeType`. `NodeType` is the protocol that represents any JSON.
     required
     init(node: NodeType) throws {
+//: Use subscript syntax to access JSON object properties and array elements. Feel free to apply a number of keys one by one and use both string and integer literals.
+//:
+//: Use `optional` property to get something that could be `null` or undefined safetly. Use `required` property to get non-optional result or error. Actually, `required` returns the same node and it is ok to skip it. It only exists to make clear intent.
+//:
+//: Use one of the `value(_:)` methods to get scalar value (number, string or boolean) or `Mappable` instance.
+//:
+//: Use the `value(transformedWith:)` method to to get instance of any other type. There are a number of predefined transforms: `Int64Transform`, `DateTransform`, etc. It is also possible to define custom transform.
+//:
+//: Use the `array()` method to get an array of nodes.
+//:
+//: Use one of the `array(_:)` convenience methods to get an array of converted instances.
+//:
+//: Use the `dictionary()` method to get a string to node dictionary.
         id = try node["id"].required.value(transformedWith: Int64Transform)
         user = try node["user"].required.value(User)
         created = try node["created_at"].required.value(transformedWith: DateTransform(dateFormat: "eee MMM dd HH:mm:ss ZZZZ yyyy"))
@@ -53,6 +73,9 @@ class Tweet: Mappable {
     }
 
 }
+
+//: ## Mappable Structure
+//: It is also possible to make `Mappable` structure. Structure allows to use non-optional constant properties.
 
 struct User: Mappable {
     let name: String
@@ -72,11 +95,16 @@ struct User: Mappable {
     }
 }
 
+//: ## Transform
+//: Transform is another way to make reusable code converting JSON to instance of some type whether it is `Mappable` or not. There are a number of inbuilt transforms such as `URLTransform` and defining custom transforms is pretty simple.
+
 struct RangeTransform: TransformType {
 
+//: One method to convert JSON represented with `NodeType` instance to instance of any other type.
     func applyToNode(node: NodeType) throws -> NSRange {
         let indices = try node.array(Int)
         guard indices.count == 2 else {
+//: Throw error to fail transform.
             throw errorWithReason("Range requires 2 indices")
         }
         return NSRange(location: indices[0], length: indices[1])
@@ -91,10 +119,14 @@ class Entity: Mappable {
 
     required
     init(node: NodeType) throws {
+//: Same way to apply custom transform as for inbuilt transforms.
         range = try node["indices"].required.value(transformedWith: RangeTransform())
     }
 
 }
+
+//: ## Mappable Types Subclassing
+//: Nothing new here, just create subclass and do not forget to call superclass initializer.
 
 class HashtagEntity: Entity {
 
@@ -155,21 +187,25 @@ class MediaEntity: URLEntity {
     
 }
 
-/*:
-## In action
+//: ## In Action
+//:
+//: Use one of following functions to get node:
+//: 1. `NodeForObject(_:)`
+//: 1. `NodeForJSONObjectWithData(_:)`
+//:
+//: _Tweet JSON has been taken from the official Twitter documentation ([GET statuses/show/:id](https://dev.twitter.com/rest/reference/get/statuses/show/:id))._
 
-_Tweet JSON has been taken from the official Twitter documentation ([GET statuses/show/:id](https://dev.twitter.com/rest/reference/get/statuses/show/:id))._
-*/
 let path = NSBundle.mainBundle().pathForResource("210462857140252672", ofType: "json")!
 let data = NSData(contentsOfFile: path)!
 let node = NodeForJSONObjectWithData(data)
 
 do {
-
+//: Convert JSON to the object of expected type.
     let tweet = try node.value(Tweet)
 
 } catch {
-
+//: All methods throw error of `MappingError` type. There are `underlyingError` tells what exactly is wrong and `path` from the root object to problem JSON.
     error
 
 }
+
