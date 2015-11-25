66f96a902d08b2c6809f680063448b30bfa69e3e
Stephen Celis <stephen.celis@gmail.com>
2015-11-25 10:28:20 -0500

    Allow node type to be inferred
    By defaulting the argument to T.self we can support type inference:
    
        let foo = try node.value(String)
        let bar: String = try node.value()

--- OMM/Mappable/NodeType+Mappable.swift
+++ OMM/Mappable/NodeType+Mappable.swift
@@ -9,15 +9,15 @@
 public
 extension NodeType {
 
-    func value<T: Mappable>(type: T.Type) throws -> T {
+    func value<T: Mappable>(type: T.Type = T.self) throws -> T {
         return try value(transformedWith: MappableTransform())
     }
 
-    func array<T: Mappable>(type: T.Type) throws -> [T] {
+    func array<T: Mappable>(type: T.Type = T.self) throws -> [T] {
         return try array().map { try $0.value(type) }
     }
 
-    func dictionary<T: Mappable>(type: T.Type) throws -> [String: T] {
+    func dictionary<T: Mappable>(type: T.Type = T.self) throws -> [String: T] {
         return try dictionary().toDictionary { try ($0, $1.value(type)) }
     }
 
