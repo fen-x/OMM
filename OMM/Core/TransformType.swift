c43f4a3c66832c9c385ec910f910e43c18c3f2ae
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 00:34:37 +0300

    Documentation for TransformType

--- OMM/Core/TransformType.swift
+++ OMM/Core/TransformType.swift
@@ -6,11 +6,16 @@
 //  Copyright © 2015 Ivan Nikitin. All rights reserved.
 //
 
+/// A type that represents transformation from node to value.
 public
 protocol TransformType {
 
+    /// Resulting value of transformation.
     typealias Value
 
+    /// Transforms node to value.
+    /// - Parameter node: Integer value.
+    /// - Returns: Result of transformation.
     func applyToNode(node: NodeType) throws -> Value
 
 }
@@ -18,6 +23,9 @@ protocol TransformType {
 public
 extension TransformType {
 
+    /// Creates an instance of `TransformError` initialized with type of `self`, it's resulting value type and given reason.
+    /// - Parameter reason: Explanation of the error reason.
+    /// - Returns: Initialized instance of `TransformError`.
     func errorWithReason(reason: String) -> ErrorType {
         return TransformError(transformType: Self.self, targetType: Self.Value.self, reason: reason)
     }
