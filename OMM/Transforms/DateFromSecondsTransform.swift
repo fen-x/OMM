cdee709daf2653762ee27f0e15d5ce8482e5e3f5
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 20:23:53 +0300

    Documentation for DateFromSecondsTransform

--- OMM/Transforms/DateFromSecondsTransform.swift
+++ OMM/Transforms/DateFromSecondsTransform.swift
@@ -9,12 +9,19 @@
 import class  Foundation.NSDate
 import struct Foundation.NSTimeInterval
 
+/// Transformation that transforms node to `NSDate` value
+/// in case of date represented as the number of **seconds** that have elapsed since 00:00:00 Coordinated Universal Time (UTC), Thursday, 1 January 1970.
 public
 struct DateFromSecondsTransform: TransformType {
 
+    /// Creates an instance of `DateFromSecondsTransform`.
     public
     init() { }
 
+    /// Transforms node to `NSDate` value.
+    /// - Parameter node: Node.
+    /// - Returns: `NSDate` instance.
+    /// - Throws: `MappingError`.
     public
     func applyToNode(node: NodeType) throws -> NSDate {
         return try NSDate(timeIntervalSince1970: node.value(NSTimeInterval))
