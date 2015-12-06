46027961006b50ccc1435420fed63b3bf44493ef
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 19:32:44 +0300

    Documentation for TransformError

--- OMM/Core/TransformError.swift
+++ OMM/Core/TransformError.swift
@@ -6,22 +6,28 @@
 //  Copyright © 2015 Ivan Nikitin. All rights reserved.
 //
 
+/// Represents transformation failure.
 public
 struct TransformError: ErrorType {
 
+    /// Type of failed transformation.
     public
     let transformType: Any.Type
 
+    /// Type of value that node tried to be transformed to.
     public
     let targetType: Any.Type
 
+    /// Explanation of the error reason.
     public
     let reason: String
 
 }
 
+/// `CustomDebugStringConvertible` conformance.
 extension TransformError: CustomDebugStringConvertible {
 
+    /// A textual representation, suitable for debugging.
     public
     var debugDescription: String {
         return "Transform \"\(transformType)\" to type \"\(targetType)\" failed with reason \"\(reason)\""
