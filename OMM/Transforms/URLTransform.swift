b305050049cdd6cb838306f1c0401ba60275e861
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 20:25:22 +0300

    Documentation for URLTransform

--- OMM/Transforms/URLTransform.swift
+++ OMM/Transforms/URLTransform.swift
@@ -8,12 +8,18 @@
 
 import class Foundation.NSURL
 
+/// Transformation that transforms node to `NSURL` value.
 public
 struct URLTransform: TransformType {
 
+    /// Creates an instance of `URLTransform`.
     public
     init() { }
 
+    /// Transforms node to `NSURL` value.
+    /// - Parameter node: Node.
+    /// - Returns: `NSURL` instance.
+    /// - Throws: `MappingError`, `TransformError` if `String` value of node does not represent valid URL.
     public
     func applyToNode(node: NodeType) throws -> NSURL {
         guard let URL = try NSURL(string: node.value(String)) else {
