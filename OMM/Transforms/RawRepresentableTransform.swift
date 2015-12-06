dfb8a3fc105a189f2c195c605af29f6870228b51
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 20:25:06 +0300

    Documentation for RawRepresentableTransform

--- OMM/Transforms/RawRepresentableTransform.swift
+++ OMM/Transforms/RawRepresentableTransform.swift
@@ -6,15 +6,21 @@
 //  Copyright © 2015 Ivan Nikitin. All rights reserved.
 //
 
+/// Transformation that transforms node to `RawRepresentable` value.
 public
 struct RawRepresentableTransform<T: RawRepresentable where T.RawValue: ScalarType>: TransformType {
 
     private
     let transform = RawRepresentableOptionalTransform<T>()
 
+    /// Creates an instance of `RawRepresentableTransform`.
     public
     init() { }
 
+    /// Transforms node to `RawRepresentable` value.
+    /// - Parameter node: Node.
+    /// - Returns: `RawRepresentable` instance.
+    /// - Throws: `MappingError`, `TransformError` if there is no value represented with raw value of node.
     public
     func applyToNode(node: NodeType) throws -> T {
         guard let value = try transform.applyToNode(node) else {
