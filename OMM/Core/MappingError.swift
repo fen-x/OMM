7ec7eb1ba4ae9354295eb00ecd2297b0eee55d6b
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 19:32:23 +0300

    Documentation for MappingError

--- OMM/Core/MappingError.swift
+++ OMM/Core/MappingError.swift
@@ -6,19 +6,24 @@
 //  Copyright © 2015 Ivan Nikitin. All rights reserved.
 //
 
+/// Represents error occured during mapping proccess.
 public
 struct MappingError: ErrorType {
 
+    /// An error that was encountered in an underlying implementation and caused the error that the receiver represents to occur.
     public
     let underlyingError: ErrorType
 
+    /// Path from the root node to node that has thrown error.
     public
     let path: [SubscriptKey]
 
 }
 
+/// `CustomDebugStringConvertible` conformance.
 extension MappingError: CustomDebugStringConvertible {
 
+    /// A textual representation, suitable for debugging.
     public
     var debugDescription: String {
         let path = self.path.map { String($0) }.joinWithSeparator(".")
