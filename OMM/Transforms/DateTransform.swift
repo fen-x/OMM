62a4c4cfaec5eb29eea397d801a4d1e28fc592f7
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 20:24:13 +0300

    Documentation for DateTransform

--- OMM/Transforms/DateTransform.swift
+++ OMM/Transforms/DateTransform.swift
@@ -12,12 +12,17 @@ import class Foundation.NSCalendar
 import var   Foundation.NSCalendarIdentifierISO8601
 import class Foundation.NSLocale
 
+/// Transformation that transforms node to `NSDate` value
+/// in case of date represented as the formatted string.
 public
 struct DateTransform: TransformType {
 
     private
     let formatter: NSDateFormatter
 
+    /// Creates an instance of `DateTransform` initilized with given date format string.
+    /// - Parameter dateFormat: Date format.
+    /// - Note: `en_US_POSIX` locale will be used for transformation.
     public
     init(dateFormat: String) {
         let formatter = NSDateFormatter()
@@ -26,11 +31,17 @@ struct DateTransform: TransformType {
         self.init(formatter: formatter)
     }
 
+    /// Creates an instance of `DateTransform` initilized with given date formatter.
+    /// - Parameter formatter: Date formatter.
     public
     init(formatter: NSDateFormatter) {
         self.formatter = formatter.copy() as! NSDateFormatter
     }
 
+    /// Transforms node to `NSDate` value.
+    /// - Parameter node: Node.
+    /// - Returns: `NSDate` instance.
+    /// - Throws: `MappingError`, `TransformError` if `String` value of node does not represent correctly formatted date.
     public
     func applyToNode(node: NodeType) throws -> NSDate {
         guard let value = try formatter.dateFromString(node.value(String)) else {
@@ -44,6 +55,8 @@ struct DateTransform: TransformType {
 public
 extension DateTransform {
 
+    /// Date transformation working with dates formatted using ISO 8601 standard.
+    /// - Note: Transformation covers only `yyyy-MM-dd'T'HH:mm:ssZZZZZ` format.
     public
     static var ISO8601: DateTransform {
         let formatter = NSDateFormatter()
