cd6ad17d53db34f6d49324abe7bcc00d1579fdd5
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 14:46:07 +0300

    Documentation for ScalarType

--- OMM/Core/ScalarType.swift
+++ OMM/Core/ScalarType.swift
@@ -6,5 +6,8 @@
 //  Copyright © 2015 Ivan Nikitin. All rights reserved.
 //
 
+/// A type that can be simply casted or bridged from raw object.
+///
+/// - Remark: Do not declare new conformances to this protocol; they will not work as expected.
 public
 protocol ScalarType { }
