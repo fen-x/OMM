50ca241b21e5378159f1ba61b599e871b44d6743
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 19:32:33 +0300

    Documentation for NullValueError

--- OMM/Core/NullValueError.swift
+++ OMM/Core/NullValueError.swift
@@ -6,11 +6,15 @@
 //  Copyright © 2015 Ivan Nikitin. All rights reserved.
 //
 
+/// Represents absence of retriving value.
+/// It means that raw object of node is an instance of `NSNull`.
 public
 struct NullValueError: ErrorType { }
 
+/// `CustomDebugStringConvertible` conformance.
 extension NullValueError: CustomDebugStringConvertible {
 
+    /// A textual representation, suitable for debugging.
     public
     var debugDescription: String {
         return "Value is \"null\""
