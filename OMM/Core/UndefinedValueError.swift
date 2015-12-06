843f134a51543d017a1fd435fd7199a86692e589
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 19:32:57 +0300

    Documentation for UndefinedValueError

--- OMM/Core/UndefinedValueError.swift
+++ OMM/Core/UndefinedValueError.swift
@@ -6,11 +6,15 @@
 //  Copyright © 2015 Ivan Nikitin. All rights reserved.
 //
 
+/// Represents absence of retriving value
+/// It means that raw object of node does not exist.
 public
 struct UndefinedValueError: ErrorType { }
 
+/// `CustomDebugStringConvertible` conformance.
 extension UndefinedValueError: CustomDebugStringConvertible {
 
+    /// A textual representation, suitable for debugging.
     public
     var debugDescription: String {
         return "Value is \"undefined\""
