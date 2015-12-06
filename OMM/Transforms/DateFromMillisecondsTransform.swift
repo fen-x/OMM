910f24453b7d7e9c6e2483c675435109ebdccbd9
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 20:23:38 +0300

    Documentation for DateFromMillisecondsTransform

--- OMM/Transforms/DateFromMillisecondsTransform.swift
+++ OMM/Transforms/DateFromMillisecondsTransform.swift
@@ -10,12 +10,19 @@ import class  Foundation.NSDate
 import class  Foundation.NSNumber
 import struct Foundation.NSTimeInterval
 
+/// Transformation that transforms node to `NSDate` value
+/// in case of date represented as the number of **milliseconds** that have elapsed since 00:00:00 Coordinated Universal Time (UTC), Thursday, 1 January 1970.
 public
 struct DateFromMillisecondsTransform: TransformType {
 
+    /// Creates an instance of `DateFromMillisecondsTransform`.
     public
     init() { }
 
+    /// Transforms node to `NSDate` value.
+    /// - Parameter node: Node.
+    /// - Returns: `NSDate` instance.
+    /// - Throws: `MappingError`.
     public
     func applyToNode(node: NodeType) throws -> NSDate {
         let number = try node.value(NSNumber)
