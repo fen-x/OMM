c368b6d525f90dd8af68671590569583aa876c53
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 20:24:29 +0300

    Documentation for IntegerTransform

--- OMM/Transforms/IntegerTransform.swift
+++ OMM/Transforms/IntegerTransform.swift
@@ -8,6 +8,7 @@
 
 import class Foundation.NSNumber
 
+/// Transformation that transforms node to integer value that accessible from `NSNumber` but can not be simply casted or bridged.
 public
 struct IntegerTransform<T: IntegerType>: TransformType {
 
@@ -19,6 +20,10 @@ struct IntegerTransform<T: IntegerType>: TransformType {
         self.transform = transform
     }
 
+    /// Transforms node to integer value.
+    /// - Parameter node: Node.
+    /// - Returns: Integer value.
+    /// - Throws: `MappingError`.
     public
     func applyToNode(node: NodeType) throws -> T {
         return try transform(node.value(NSNumber))
@@ -26,41 +31,49 @@ struct IntegerTransform<T: IntegerType>: TransformType {
 
 }
 
+/// Transformation that transforms node to `Int8` value (char value).
 public
 var Int8Transform: IntegerTransform<Int8> {
     return IntegerTransform { $0.charValue }
 }
 
+/// Transformation that transforms node to `Int16` value (short value).
 public
 var Int16Transform: IntegerTransform<Int16> {
     return IntegerTransform { $0.shortValue }
 }
 
+/// Transformation that transforms node to `Int32` value (int value).
 public
 var Int32Transform: IntegerTransform<Int32> {
     return IntegerTransform { $0.intValue }
 }
 
+/// Transformation that transforms node to `Int64` value (long long value).
 public
 var Int64Transform: IntegerTransform<Int64> {
     return IntegerTransform { $0.longLongValue }
 }
 
+/// Transformation that transforms node to `UInt8` value (unsigned char value).
 public
 var UInt8Transform: IntegerTransform<UInt8> {
     return IntegerTransform { $0.unsignedCharValue }
 }
 
+/// Transformation that transforms node to `UInt16` value (unsigned short value).
 public
 var UInt16Transform: IntegerTransform<UInt16> {
     return IntegerTransform { $0.unsignedShortValue }
 }
 
+/// Transformation that transforms node to `UInt32` value (unsigned int value).
 public
 var UInt32Transform: IntegerTransform<UInt32> {
     return IntegerTransform { $0.unsignedIntValue }
 }
 
+/// Transformation that transforms node to `UInt64` value (unsigned long long value).
 public
 var UInt64Transform: IntegerTransform<UInt64> {
     return IntegerTransform { $0.unsignedLongLongValue }
