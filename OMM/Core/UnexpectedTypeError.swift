b31fcf224fa8b1fd81a1f516f959a27bb1f018be
Ivan Nikitin <fenix661@gmail.com>
2015-12-06 19:33:10 +0300

    Documentation for UnexpectedTypeError

--- OMM/Core/UnexpectedTypeError.swift
+++ OMM/Core/UnexpectedTypeError.swift
@@ -6,19 +6,24 @@
 //  Copyright © 2015 Ivan Nikitin. All rights reserved.
 //
 
+/// Represents the situation when raw object could not be casted or bridget to an expected type.
 public
 struct UnexpectedTypeError: ErrorType {
 
+    /// Expected type necessary to complete requested operation.
     public
     let expectedType: Any.Type
 
+    /// Actual type of raw object.
     public
     let actualType: Any.Type
     
 }
 
+/// `CustomDebugStringConvertible` conformance.
 extension UnexpectedTypeError: CustomDebugStringConvertible {
 
+    /// A textual representation, suitable for debugging.
     public
     var debugDescription: String {
         return "\"\(expectedType)\" is expected but \"\(actualType)\" found"
