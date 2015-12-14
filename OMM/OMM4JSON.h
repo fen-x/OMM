e8bc525ffea7be41cddadd99fc118e3ffc9ebf96
Ivan Nikitin <fenix661@gmail.com>
2015-12-15 01:07:09 +0300

    Add OMM4JSON target to get only JSON supported through Carthage

---
+++ OMM/OMM4JSON.h
@@ -0,0 +1,15 @@
+//
+//  OMM4JSON.h
+//  OMM4JSON
+//
+//  Created by Ivan Nikitin on 15/12/15.
+//  Copyright © 2015 Ivan Nikitin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OMM4JSON.
+FOUNDATION_EXPORT double OMM4JSONVersionNumber;
+
+//! Project version string for OMM4JSON.
+FOUNDATION_EXPORT const unsigned char OMM4JSONVersionString[];
