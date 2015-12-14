680742e7d67d39a925da01e21d7b30be9e441466
Ivan Nikitin <fenix661@gmail.com>
2015-12-15 01:24:56 +0300

    Add OMM4JSON target to get only property list supported through Carthage

---
+++ OMM/OMM4PropertyList.h
@@ -0,0 +1,15 @@
+//
+//  OMM4PropertyList.h
+//  OMM4PropertyList
+//
+//  Created by Ivan Nikitin on 15/12/15.
+//  Copyright © 2015 Ivan Nikitin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OMM4PropertyList.
+FOUNDATION_EXPORT double OMM4PropertyListVersionNumber;
+
+//! Project version string for OMM4PropertyList.
+FOUNDATION_EXPORT const unsigned char OMM4PropertyListVersionString[];
