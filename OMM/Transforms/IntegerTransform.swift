c3a4b664b65fedc32beb530dcafbfd1971d0e4c7
Ivan Nikitin <fenix661@gmail.com>
2017-10-04 23:47:07 +0300

    Use key path instead of closure for integer transform

--- OMM/Transforms/IntegerTransform.swift
+++ OMM/Transforms/IntegerTransform.swift
@@ -11,11 +11,11 @@ public
 struct IntegerTransform<T: BinaryInteger>: Transform {
 
     private
-    let transform: (NSNumber) -> T
+    let keyPath: KeyPath<NSNumber, T>
 
     fileprivate
-    init(transform: @escaping (NSNumber) -> T) {
-        self.transform = transform
+    init(keyPath: KeyPath<NSNumber, T>) {
+        self.keyPath = keyPath
     }
 
     /// Transforms node to integer value.
@@ -25,7 +25,7 @@ struct IntegerTransform<T: BinaryInteger>: Transform {
     /// - Throws: `MappingError`.
     public
     func apply(to node: Node) throws -> T {
-        return try transform(node.value(NSNumber.self))
+        return try node.value(NSNumber.self)[keyPath: keyPath]
     }
 
 }
@@ -33,47 +33,47 @@ struct IntegerTransform<T: BinaryInteger>: Transform {
 /// Transformation that transforms node to `Int8` value (char value).
 public
 var int8Transform: IntegerTransform<Int8> {
-    return IntegerTransform { $0.int8Value }
+    return IntegerTransform(keyPath: \.int8Value)
 }
 
 /// Transformation that transforms node to `Int16` value (short value).
 public
 var int16Transform: IntegerTransform<Int16> {
-    return IntegerTransform { $0.int16Value }
+    return IntegerTransform(keyPath: \.int16Value)
 }
 
 /// Transformation that transforms node to `Int32` value (int value).
 public
 var int32Transform: IntegerTransform<Int32> {
-    return IntegerTransform { $0.int32Value }
+    return IntegerTransform(keyPath: \.int32Value)
 }
 
 /// Transformation that transforms node to `Int64` value (long long value).
 public
 var int64Transform: IntegerTransform<Int64> {
-    return IntegerTransform { $0.int64Value }
+    return IntegerTransform(keyPath: \.int64Value)
 }
 
 /// Transformation that transforms node to `UInt8` value (unsigned char value).
 public
 var uint8Transform: IntegerTransform<UInt8> {
-    return IntegerTransform { $0.uint8Value }
+    return IntegerTransform(keyPath: \.uint8Value)
 }
 
 /// Transformation that transforms node to `UInt16` value (unsigned short value).
 public
 var uint16Transform: IntegerTransform<UInt16> {
-    return IntegerTransform { $0.uint16Value }
+    return IntegerTransform(keyPath: \.uint16Value)
 }
 
 /// Transformation that transforms node to `UInt32` value (unsigned int value).
 public
 var uint32Transform: IntegerTransform<UInt32> {
-    return IntegerTransform { $0.uint32Value }
+    return IntegerTransform(keyPath: \.uint32Value)
 }
 
 /// Transformation that transforms node to `UInt64` value (unsigned long long value).
 public
 var uint64Transform: IntegerTransform<UInt64> {
-    return IntegerTransform { $0.uint64Value }
+    return IntegerTransform(keyPath: \.uint64Value)
 }
